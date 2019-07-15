2280ea6fa05873397b03f97ebf76e847289a4744
Mattkee <tkee@hotmail.fr>
2019-07-15 12:52:42 +0200

    Update Constant.swift

--- Reciplease/Supporting Files/Constant.swift
+++ Reciplease/Supporting Files/Constant.swift
@@ -11,8 +11,8 @@ import Foundation
 struct Constant {
     // MARK: - General Constant
     static let titleAlert = "Echec Appel réseau"
-    static let yummlyAPIID = "11c8630f"
-    static let yummlyAPIKey = "fc109c0cebd13ea423026392a404ea49"
+    static let yummlyAPIID = "********" // Put your Yummly API ID
+    static let yummlyAPIKey = "********" // Put your Yummly API Key
 
 //    static let cookingChoice = ["American", "Italian", "Asian", "Mexican", "Southern & Soul Food", "French", "Southwestern", "Barbecue", "Indian", "Chinese", "Cajun & Creole", "English", "Mediterranean", "Greek", "Spanish", "German", "Thai", "Moroccan", "Irish", "Japanese", "Cuban", "Hawaiin", "Swedish", "Hungarian", "Portugese"]
 //    static let diets = ["Lacto vegetarian", "Ovo vegetarian", "Pescetarian", "Vegan", "Vegetarian"]
